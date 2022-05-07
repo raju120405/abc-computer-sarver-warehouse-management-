f4b8be4faafefe8caa313b7edcdbd8de9c43adfe
raju120405 <raju120405@gmail.com>
2022-05-07 10:49:42 +0600

    name price connect

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
-require('dotenv').config()
-app.use(cors())
+require('dotenv').config();
+app.use(cors());
+app.use(express.json())
 const port = process.env.PORT || 5000;
 
 
@@ -16,6 +17,10 @@ async function run() {
     try {
       await client.connect();
       console.log('db connect')
+      app.post('/uploadPd',async(req,res)=>{
+          const product = req.body;
+          console.log(product)
+      })
     } finally {
       await client.close();
     }
