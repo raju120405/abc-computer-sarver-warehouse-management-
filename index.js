3a879ef29ddef9244c4588edf684de547585147b
raju120405 <raju120405@gmail.com>
2022-05-07 09:43:43 +0600

    db connected

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+require('dotenv').config()
+app.use(cors())
+const port = process.env.PORT || 5000;
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cavll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+      await client.connect();
+      console.log('db connect')
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send('running my node abc computer server')
+});
+
+app.listen(port,()=>{
+    console.log('abc computer server is running');
+})
